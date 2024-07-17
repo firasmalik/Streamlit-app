fa2dd80b0daed2d02ce7f87a3163aeee79a8b382
Firas Hamid <firasmalik@gmail.com>
2024-07-17 12:59:13 +0000

    Streamlit Deployment 4

--- Classification.py
+++ Classification.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import pandas as pd
-from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+
+# Load the iris dataset
+
+
+
 
 @st.cache_data
 def load_data():
-    iris = load_iris()
+    iris = datasets.load_iris()
     df = pd.DataFrame(iris.data, columns=iris.feature_names)
     df['species'] = iris.target
     return df, iris.target_names
